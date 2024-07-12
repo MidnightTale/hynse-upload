1c2b47f6361b08fd01a0521bb09f321ec17fab1f
MidnightTale <125941391+MidnightTale@users.noreply.github.com>
2024-07-13 02:30:31 +0700

    Delete example_page directory

--- example_page/js/upload/disableUpload.js
+++
@@ -1,7 +0,0 @@
-import { setUploading, uploading } from './uploadState.js';
-
-export function disableUpload() {
-    setUploading(true);
-    document.getElementById('fileInput').disabled = true;
-    document.getElementById('dropZone').classList.add('disabled');
-}
